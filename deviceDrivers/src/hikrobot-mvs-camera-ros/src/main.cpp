a9d7528f2fe89f23989e38b19e3ac3d5b140667f
Charles <lililililock@gmail.com>
2023-12-25 21:40:33 +0800

    add XT32M2X angle correction file add launch file for smartscanner add rviz config for smartscanner

--- deviceDrivers/src/hikrobot-mvs-camera-ros/src/main.cpp
+++ deviceDrivers/src/hikrobot-mvs-camera-ros/src/main.cpp
@@ -52,8 +52,8 @@ int main(int argc, char **argv)
     // image_transport::CameraPublisher image_pub = main_cam_image.advertiseCamera("/hikrobot_camera/rgb", 1000);
     // 目前实测使用image_transport::Publisher发布的话题中compressed仍然无法订
     // *20220912 多次验证后，目前已可订阅compressed*
-    image_transport::Publisher image_pub = main_cam_image.advertise("/hikrobot_camera/rgb", 1000);
-    ros::Publisher camera_info_pub = hikrobot_camera.advertise<sensor_msgs::CameraInfo>("/hikrobot_camera/camera_info", 1000);
+    image_transport::Publisher image_pub = main_cam_image.advertise("/hikrobot_camera/rgb", 5000);
+    ros::Publisher camera_info_pub = hikrobot_camera.advertise<sensor_msgs::CameraInfo>("/hikrobot_camera/camera_info", 5000);
 
     // TODO Use threads to do dynamic reconfigure
     // dynamic_reconfigure::Server<hikrobot_camera::cameraConfig> server;
